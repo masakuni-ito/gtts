5ff918f375df77c6a166a110af65281ca03aca14
Masakuni Ito <masakuni.ito.19@gmail.com>
2020-11-29 19:57:50 +0900

    Add language parameter to save_to_file

--- README.md
+++ README.md
@@ -6,7 +6,17 @@ It requires an Internet connection.
 ## Example
 
 ```rust
-save_to_file("Hello world!", "test");
+save_to_file("Hello world!", "test", "en");
+
+save_to_file("こんにちは!", "test", "ja");
 ```
 
-License: MIT
+## License
+
+Copyright (c) 2020 Masakuni Ito
+[MIT](https://opensource.org/licenses/MIT)
+
+This project was abstracted from Mubelotix's [gtts](https://github.com/Mubelotix/gtts), which is subject to the same license.
+Here is the original copyright notice for gtts:
+
+MIT
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 //! This is a very small crate using google text to speech (GTTS).
 //! It requires an Internet connection.
-//! 
+//!
 //! # Example
-//! 
+//!
 //! ```
 //! # use gtts::save_to_file;
-//! save_to_file("Hello world!", "test");
+//! save_to_file("Hello world!", "test", "en");
 //! ```
 
 use percent_encoding::{AsciiSet, utf8_percent_encode, CONTROLS};
@@ -17,11 +17,11 @@ const FRAGMENT: &AsciiSet = &CONTROLS.add(b' ').add(b'"').add(b'<').add(b'>').ad
 
 /// Use google translate to get a audio version of your text and save this in a file.
 /// Return true if everything succeed.
-pub fn save_to_file(text: &str, filename: &str) -> bool {
+pub fn save_to_file(text: &str, filename: &str, language: &str) -> bool {
     let len = text.len();
     let text = utf8_percent_encode(text, FRAGMENT).to_string();
 
-    if let Ok(rep) = get(format!("https://translate.google.fr/translate_tts?ie=UTF-8&q={}&tl=en&total=1&idx=0&textlen={}&tl=en&client=tw-ob", text, len)).send() {
+    if let Ok(rep) = get(format!("https://translate.google.com/translate_tts?ie=UTF-8&q={}&total=1&idx=0&textlen={}&tl={}&client=tw-ob", text, len, language)).send() {
         if let Ok(mut file) = File::create(filename) {
             let bytes = rep.as_bytes();
             if bytes.len() > 0 {
@@ -40,7 +40,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test() {
-        assert!(save_to_file("Hello world!", "test"));
+    fn test_in_en() {
+        assert!(save_to_file("Hello world!", "test_en.mp3", "en"));
+    }
+
+    #[test]
+    fn test_in_ja() {
+        assert!(save_to_file("こんにちは!", "test_ja.mp3", "ja"));
     }
 }
